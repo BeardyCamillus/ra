69bde455e29bebc6f8a3b91c04903aa135701e7f
Kamil Bikineev <beardy.camillus@gmail.com>
2023-11-26 21:18:10 +0200

    hex2rgb fix

--- forms/hex2rgb/src/App.tsx
+++ forms/hex2rgb/src/App.tsx
@@ -5,17 +5,20 @@ function App() {
     const [input, setInput] = useState<string>('');
     const [output, setOutput] = useState<string>('');
 
+    const hexRegex = /^[0-9A-F]{6}[0-9a-f]{0,2}$/i;
+
     const inputChangeHandler = (event: ChangeEvent<HTMLInputElement>): void => {
         const value = event.target.value.slice(1, 7);
         setInput(value);
- 
-        const r = parseInt(value.slice(0, 2), 16);
-        const g = parseInt(value.slice(2, 4), 16);
-        const b = parseInt(value.slice(4, 6), 16);
-        if (!r && !g && !b) {
-            return setOutput('Ошибка!')
+
+        if (hexRegex.test(value)) {
+            const r = parseInt(value.slice(0, 2), 16);
+            const g = parseInt(value.slice(2, 4), 16);
+            const b = parseInt(value.slice(4, 6), 16);
+            setOutput(`rgb(${r}, ${g}, ${b})`);
+        } else {
+            setOutput('Ошибка!');
         }
-        setOutput(`rgb(${r}, ${g}, ${b})`);
     }
 
     return (
